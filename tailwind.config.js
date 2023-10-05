136b4083d65aeb0297176b537b3126526e771737
adliharahap <adliharahap1123@gmail.com>
2023-10-05 11:33:03 +0700

    done membuat navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"]
+      },
+      backgroundColor: {
+        grayblack: "#222222",
+      }
+    },
   },
   plugins: [],
 }
